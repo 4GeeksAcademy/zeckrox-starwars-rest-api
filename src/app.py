692017109a3b93a6e1be93bb89eda2d3f16d9653
Stefano <tefanoboschetti@gmail.com>
2023-09-25 06:00:43 +0000

    ✨GET,POST,DELETE for People, Planets, Favorites🎉

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -32,18 +32,258 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route("/")# Aquí definimos el primer path de la API: GET /
+def hello():
+    return "Hello World!"  # Aquí flask devolverá "Hello World, esto podría ser un string HTML o un string JSON.
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#PEOPLE
 
-    return jsonify(response_body), 200
+#GET ALL CHARACTERS
+@app.route("/people", methods=["GET"])
+def get_people():
+    try:
+        all_people = People.query.all()
+        return [character.serialize() for character in all_people]
+    
+    except ValueError as err:
+        return {"message": "failed to retrieve planet " + err}, 500
+
+#GET A CHARACTER BY ID
+@app.route("/people/<int:people_id>", methods=["GET"])
+def get_people_id(people_id):
+    try:
+        selected_people = People.query.get(people_id) or None
+        if selected_people == None:
+            return {"message": f"Character with id {people_id} does not exist"}, 400
+        else:
+            return selected_people.serialize()
+        
+    except ValueError as err:
+        return {"message": "failed to retrieve people " + err}, 500
+
+#POST A CHARACTER
+@app.route("/people", methods=["POST"])
+def post_people():
+    try:
+        body = request.get_json()
+
+        name = body.get("name", None)
+        gender = body.get ("gender", None)
+        birth_year = body.get ("birth_year", None)
+        hair_color = body.get ("hair_color", None)
+        skin_color = body.get ("skin_color", None)
+        eye_color = body.get ("eye_color", None)
+        height = body.get ("height", None)
+        mass = body.get ("mass", None)
+        
+        people_required_info = (name, birth_year, skin_color, height, mass)
+        for key in people_required_info:
+            if key == None: return {"message": "Some field is missing in request body"}, 400
+        
+        new_people = People( name=name, gender=gender, birth_year=birth_year, hair_color=hair_color, skin_color=skin_color, eye_color=eye_color, height=height, mass=mass )
+        db.session.add(new_people)
+        db.session.commit()
+        return new_people.serialize(), 200
+    
+    except ValueError as err:
+        return {"message": "failed to retrieve planet " + err}, 500
+    
+#DELETE A CHARACTER
+@app.route("/people/<int:people_id>", methods=["DELETE"])
+def delete_people(people_id):
+    try:
+        selected_people = People.query.get(people_id) or None
+        if selected_people == None:
+            return {"message": f"Planet with id {people_id} does not exist"}, 400
+        else:
+            db.session.delete(selected_people)
+            db.session.commit()
+            return {"message": f"{selected_people.serialize()['name']} has been deleted"}, 200
+        
+    except ValueError as err:
+        return {"message": "failed to retrieve people " + err}, 500
+
+
+#PLANETS
+
+#GET ALL PLANETS
+@app.route("/planets", methods=["GET"])
+def get_planets():
+    try:
+        all_planets = Planet.query.all()
+        return [planet.serialize() for planet in all_planets]
+    
+    except ValueError as err:
+        return {"message": "failed to retrieve planet " + err}, 500
+
+#GET A SPECIFIC PLANET BY ID
+@app.route("/planets/<int:planet_id>", methods=["GET"])
+def get_planet_id(planet_id):
+    try:
+        selected_planet = Planet.query.get(planet_id) or None
+        if selected_planet == None:
+            return {"message": f"Planet with id {planet_id} does not exist"}, 400
+        else:
+            return selected_planet.serialize()
+        
+    except ValueError as err:
+        return {"message": "failed to retrieve planet " + err}, 500
+
+#POST A PLANET
+@app.route("/planets", methods=["POST"])
+def post_planet():
+    try:
+        body = request.get_json()
+        
+        name = body.get("name", None)
+        terrain = body.get ("terrain", None)
+        climate = body.get ("climate", None)
+        population = body.get ("population", None)
+        gravity = body.get ("gravity", None)
+        diameter = body.get ("diameter", None)
+
+        planet_required_info = (name, terrain, climate, population, gravity, diameter)
+        for key in planet_required_info:
+            if key == None: return {"message": "Some field is missing in request body"}, 400
+        
+        new_planet = Planet( name=name, terrain=terrain, climate=climate, population=population, gravity=gravity, diameter=diameter )
+        db.session.add(new_planet)
+        db.session.commit()
+        return new_planet.serialize(), 200
+    
+    except ValueError as err:
+        return {"message": "failed to retrieve planet " + err}, 500
+
+#DELETE A PLANET
+@app.route("/planets/<int:planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    try:
+        selected_planet = Planet.query.get(planet_id) or None
+        if selected_planet == None:
+            return {"message": f"Planet with id {planet_id} does not exist"}, 400
+        else:
+            db.session.delete(selected_planet)
+            db.session.commit()
+            return {"message": f"{selected_planet.serialize()['name']} has been deleted"}, 200
+        
+    except ValueError as err:
+        return {"message": "failed to retrieve people " + err}, 500
+
+
+#USERS
+
+#GET ALL USERS
+@app.route("/users", methods=["GET"])
+def get_users():
+    try:
+        all_users = User.query.all()
+        return [user.serialize() for user in all_users]
+
+    except ValueError as err:
+        return {"message": "failed to retrieve planet " + err}, 500
+
+#GET USER BY ID 
+@app.route("/users/<int:user_id>", methods=["GET"])
+def get_user_id(user_id):
+    try:
+        selected_user = User.query.get(user_id) or None
+        if selected_user == None:
+            return {"message": f"User with id {user_id} does not exist"}
+        else:
+            return selected_user.serialize()
+    
+    except ValueError as err:
+        return {"message": "failed to retrieve user " + err}, 500
+
+
+#FAVORITES
+
+#GET ALL FAVORITES FROM USER
+@app.route("/users/<int:user_id>/favorites", methods=["GET"])
+def get_favorites(user_id):
+    try:
+        user_favorites = Favorite.query.filter(Favorite.user_id == user_id)
+        return [favorite.serialize() for favorite in user_favorites]
+        
+    except ValueError as err:
+        return {"message": "failed to retrieve user " + err}, 500
+    
+#FAVORITE PLANET
+
+#POST A FAVORITE PLANET FOR AN USER
+@app.route("/users/<int:user_id>/favorites/planet/<int:planet_id>", methods=["POST"])
+def post_favorite_planet(user_id, planet_id):   
+    try:
+        planet = Planet.query.get(planet_id)
+        same_planet = Favorite.query.filter(Favorite.planet_id == planet_id, Favorite.user_id == user_id)
+        same_planet = [planet.serialize() for planet in same_planet]
+        if same_planet:
+            return {"message": f"{same_planet[0]['planet']} is already on favorites"}
+        elif not planet:
+            return {"message": f"People with ID:{planet_id} doesn't exist"}
+        else:
+            new_favorite = Favorite( user_id=user_id, planet_id=planet_id, planet=planet )
+            db.session.add(new_favorite)
+            db.session.commit()
+            return new_favorite.serialize() , 200
+    
+    except ValueError as err:
+        return {"message": "failed to retrieve planet " + err}, 500
+
+#DELETE A FAVORITE PLANET FOR AN USER
+@app.route("/users/<int:user_id>/favorites/planet/<int:planet_id>", methods=["DELETE"])
+def delete_favorites_planet(user_id, planet_id):
+    try:
+        selected_favorite = Favorite.query.filter(Favorite.user_id == user_id, Favorite.planet_id == planet_id)
+        for favorite in selected_favorite:
+            db.session.delete(favorite)
+        db.session.commit()
+
+        user_favorites = Favorite.query.filter(Favorite.user_id == user_id)
+        return [favorite.serialize() for favorite in user_favorites]
+        
+    except ValueError as err:
+        return {"message": "failed to retrieve user " + err}, 500
+    
+
+#FAVORITE CHARACTER
+
+#POST A FAVORITE CHARACTER FOR AN USER
+@app.route("/users/<int:user_id>/favorites/people/<int:people_id>", methods=["POST"])
+def post_favorite_people(user_id, people_id):
+    try:
+        character = People.query.get(people_id)
+        same_character = Favorite.query.filter(Favorite.people_id == people_id, Favorite.user_id == user_id)
+        same_character = [people.serialize() for people in same_character]
+        if same_character:
+            return {"message": f"{same_character[0]['people']} is already on favorites"}
+        elif not character:
+            return {"message": f"People with ID:{people_id} doesn't exist"}
+        else:
+            new_favorite = Favorite( user_id=user_id, people_id=people_id, people=character )
+            db.session.add(new_favorite)
+            db.session.commit()
+            return new_favorite.serialize() , 200
+    
+    except ValueError as err:
+        return {"message": "failed to retrieve planet " + err}, 500
+
+#DELETE A FAVORITE CHARACTER FOR AN USER
+@app.route("/users/<int:user_id>/favorites/people/<int:people_id>", methods=["DELETE"])
+def delete_favorites_people(user_id, people_id):
+    try:
+        selected_favorite = Favorite.query.filter(Favorite.user_id == user_id, Favorite.people_id == people_id)
+        for favorite in selected_favorite:
+            db.session.delete(favorite)
+        db.session.commit()
+
+        user_favorites = Favorite.query.filter(Favorite.user_id == user_id)
+        return [favorite.serialize() for favorite in user_favorites]
+        
+    except ValueError as err:
+        return {"message": "failed to retrieve user " + err}, 500
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,89 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(12), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return self.username
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
+            "password": self.password
             # do not serialize the password, its a security breach
+        }
+    
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False )
+    gender = db.Column(db.String(10))
+    birth_year = db.Column(db.String(10), nullable=False )
+    hair_color = db.Column(db.String(20))
+    skin_color = db.Column(db.String(20), nullable=False )
+    eye_color = db.Column(db.String(20) )
+    height = db.Column(db.Integer, nullable=False )
+    mass = db.Column(db.Integer, nullable=False )
+
+    def __repr__(self):
+        return self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "height": self.height,
+            "mass": self.mass
+        }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True )
+    name = db.Column(db.String, nullable=False, unique=True )
+    terrain = db.Column(db.String, nullable=False )
+    climate = db.Column(db.String, nullable=False )
+    population = db.Column(db.Integer, nullable=False )
+    gravity = db.Column(db.String, nullable=False )
+    diameter = db.Column(db.String, nullable=False )
+
+    def __repr__(self):
+        return self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "climate": self.climate,
+            "population": self.population,
+            "gravity": self.gravity,
+            "diameter": self.diameter
+        }
+    
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False )
+    user = db.relationship("User")
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id") )
+    people = db.relationship("People")
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id") )
+    planet = db.relationship("Planet")
+
+    def __repr__(self):
+        return f"User {self.user_id} favorites"
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "people_id": self.people_id if self.people else None,
+            "people": self.people.name if self.people else None,
+            "planet_id": self.planet_id if self.planet else None,
+            "planet": self.planet.name if self.planet else None
         }
